bd2dcbfefa7abec61e11c5e0d63a5b1ad68adcd3
nanthakumar-dev <nantha656565@gmail.com>
2025-03-10 01:26:56 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Simple route that responds with 'Hello, World!'
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Another route to show how we can handle different paths
+app.get('/about', (req, res) => {
+  res.send('This is the About page!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
